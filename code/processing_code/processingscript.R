e83c222c594c1af65c438d37797e14a5e0df711f
Brent Cameron <wwbd28@gmail.com>
2021-10-16 02:27:10 -0400

    Processing Script Cleaning Data/Edits

--- code/processing_code/processingscript.R
+++ code/processing_code/processingscript.R
@@ -11,52 +11,35 @@ library(here) #to set paths
 
 #path to data
 #note the use of the here() package and not absolute paths
-data_location <- here::here("data","raw_data","exampledata.xlsx")
+data_location <- here::here("data","raw_data","SympAct_Any_Pos.Rda")
 
 #load data. 
-#note that for functions that come from specific packages (instead of base R)
-# I often specify both package and function like so
-#package::function() that's not required one could just call the function
-#specifying the package makes it clearer where the function "lives",
-#but it adds typing. You can do it either way.
-rawdata <- readxl::read_excel(data_location)
+rawdata <- readRDS(data_location)
 
 #take a look at the data
 dplyr::glimpse(rawdata)
 
-#dataset is so small, we can print it to the screen.
-#that is often not possible.
-print(rawdata)
+#Remove all of the following variables: Score, Total, FluA, FluB, Dxname, 
+#Activity, Unique.Visit
+processeddata = rawdata[ , !(names(rawdata) %in% c("DxName1",
+           "DxName2", "DxName3", "DxName4", "DxName5", "Unique.Visit",
+           "ActivityLevel","ActivityLevelF","RapidFluA", "RapidFluB","PCRFluA",
+           "PCRFluB","TransScore1","TransScore1F","TransScore2","TransScore2F",
+           "TransScore3","TransScore3F","TransScore4","TransScore4F",
+           "ImpactScore","ImpactScore2","ImpactScore3","ImpactScoreF",
+           "ImpactScore2F","ImpactScore3F","ImpactScoreFD","TotalSymp1",
+           "TotalSymp1","TotalSymp1F","TotalSymp2","TotalSymp3"))]
 
-# looks like we have measurements for height (in centimeters) and weight (in kilogram)
+#Check to see if any presence of NA observations
+na.fail(processeddata)
 
-# there are some problems with the data: 
-# There is an entry which says "sixty" instead of a number. 
-# Does that mean it should be a numeric 60? It somehow doesn't make
-# sense since the weight is 60kg, which can't happen for a 60cm person (a baby)
-# Since we don't know how to fix this, we need to remove the person.
-# This "sixty" entry also turned all Height entries into characters instead of numeric.
-# We need to fix that too.
-# Then there is one person with a height of 6. 
-# that could be a typo, or someone mistakenly entered their height in feet.
-# Since we unfortunately don't know, we'll have to remove this person.
-# similarly, there is a person with weight of 7000, which is impossible,
-# and one person with missing weight.
-# to be able to analyze the data, we'll remove those 5 individuals
+#There are, now we check the data to find NA observations
+is.na(processeddata)
 
-# this is one way of doing it. Note that if the data gets updated, 
-# we need to decide if the thresholds are ok (newborns could be <50)
-
-processeddata <- rawdata %>% dplyr::filter( Height != "sixty" ) %>% 
-                             dplyr::mutate(Height = as.numeric(Height)) %>% 
-                             dplyr::filter(Height > 50 & Weight < 1000)
-
-# save data as RDS
-# I suggest you save your processed and cleaned data as RDS or RDA/Rdata files. 
-# This preserves coding like factors, characters, numeric, etc. 
-# If you save as CSV, that information would get lost.
-# See here for some suggestions on how to store your processed data:
-# http://www.sthda.com/english/wiki/saving-data-into-r-data-format-rds-and-rdata
+#Omit all NA observations for ease of data utilization
+ processeddata <-na.omit(processeddata)
+ 
+ processeddata
 
 # location to save file
 save_data_location <- here::here("data","processed_data","processeddata.rds")
